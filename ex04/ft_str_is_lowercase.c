93089218e0feb46a889d4ae6902802d6abef1bda
Emillilyn <emilly.vavrinu@icloud.com>
2026-05-21 22:27:45 +0200

    Comment
    I have commented forgoten function that links to main function

--- ex04/ft_str_is_lowercase.c
+++ ex04/ft_str_is_lowercase.c
@@ -25,7 +25,7 @@ int	ft_str_is_lowercase(char *str)
 	}
 	return (1);
 }
-
+/*
 void	ft_putstr(char *str)
 {
 	int	i;
@@ -36,7 +36,7 @@ void	ft_putstr(char *str)
 		write(1, &str[i], 1);
 		i++;
 	}
-}
+} */
 
 // int	main(void)
 // {
